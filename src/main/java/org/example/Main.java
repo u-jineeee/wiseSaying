b8a4e9819d8280f0849ac790252df74a76f5785a
ujineeee <yujin11006@naver.com>
2023-03-02 09:44:38 +0900

    5 step

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,14 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int num = 1;
+        int num = 0;
+        List<WiseSaying> list = new ArrayList<>();
+
         System.out.println("== 명언 앱 ==");
         while(true){
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,8 +22,15 @@ public class Main {
                 String wiseSaying = br.readLine();
                 System.out.print("작가 : ");
                 String author = br.readLine();
-                System.out.println(num + "번 명령이 등록되었습니다.");
-                num++;
+                System.out.println(++num + "번 명령이 등록되었습니다.");
+                list.add(new WiseSaying(wiseSaying, author, num));
+            }
+            else if(input.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for(int i = num; i > 0; i--){
+                    System.out.printf("%d / %s / %s\n", list.get(i - 1).number, list.get(i - 1).author, list.get(i - 1).wiseSaying);
+                }
             }
         }
     }
@@ -29,10 +40,9 @@ class WiseSaying {
     String wiseSaying;
     String author;
     int number;
-    static int count;
-    WiseSaying(String _wiseSaying, String _author){
+    WiseSaying(String _wiseSaying, String _author, int _number){
         this.wiseSaying = _wiseSaying;
         this.author = _author;
-        this.number = ++count;
+        this.number = _number;
     }
 }
